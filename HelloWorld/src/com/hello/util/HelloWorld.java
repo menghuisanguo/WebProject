cb4de1faabdc84a6798b4605e4111b3bb672c9d5
Administrator <764858359@qq.com>
2016-07-20 16:04:53 +0800

    添加HelloWorld类文件

---
+++ HelloWorld/src/com/hello/util/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hello.util;
+
+/**
+ * Created by Administrator on 2016/7/20.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
