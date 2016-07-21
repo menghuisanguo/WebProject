3ca4e18e13eb48f9894433f88553fc11129d3e7c
Administrator <764858359@qq.com>
2016-07-21 10:53:13 +0800

    修改

--- HelloWorld/src/com/hello/util/HelloWorld.java
+++ HelloWorld/src/com/hello/util/HelloWorld.java
@@ -6,5 +6,6 @@ package com.hello.util;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("test1");
+        System.out.println("sddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
     }
 }
